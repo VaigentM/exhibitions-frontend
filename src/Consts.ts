cca04b59a947e4517c0d44d86707ba26c02db636
YarikMix <114735039+VaigentM@users.noreply.github.com>
2024-01-18 14:23:06 +0300

    Init

---
+++ src/Consts.ts
@@ -0,0 +1,26 @@
+export const requestTime = 1000
+
+
+export const iThematicsMock = [
+	{
+		id: 1,
+		name: "Робототехника",
+		status: 1,
+		description: "Робототехника",
+		image: ""
+	},
+	{
+		id: 2,
+		name: "Аддитивное производство",
+		status: 1,
+		description: "Аддитивное производство",
+		image: ""
+	},
+	{
+		id: 3,
+		name: "Искусственный интеллект",
+		status: 1,
+		description: "Искусственный интеллект",
+		image: ""
+	},
+]
---
+++ src/Types.ts
@@ -0,0 +1,7 @@
+export interface Thematic {
+    id: number,
+    name: string,
+    status: number,
+    description: string,
+    image: string
+}
